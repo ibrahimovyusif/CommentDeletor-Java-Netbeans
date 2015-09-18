5158377d7a76d29bdbbc216ff9788df30a6ccce2
yusifibrahimov <yusifibrahimov@hotmail.com>
2015-09-18 07:44:22 +0100

    update java files

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,71 @@
+package javaapplication1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+public class JavaApplication1 {
+      
+  public static void main(String[] args) {
+  // TODO Auto-generated method stub
+  BufferedReader br = null;
+  Writer writer = null;
+  String[] file = null;
+  String inputPath, outputPath;
+ 
+  inputPath = args[0]; 
+  outputPath = args[1];
+  
+  try 
+  {
+    String sCurrentLine;
+    //String REGEX = "(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)";
+    String REGEX = "\\/\\*.*\\*\\/";
+    
+    File inputFile = new File(inputPath);
+    br = new BufferedReader(new FileReader(inputFile));
+    
+    File outputFile = new File(outputPath);
+    outputFile.createNewFile(); 
+			 
+    writer = new BufferedWriter(new OutputStreamWriter(
+      new FileOutputStream(outputFile), "utf-8"));
+
+    while ((sCurrentLine = br.readLine()) != null) 
+    {
+      String REPLACE = "";
+
+      Pattern p = Pattern.compile(REGEX);
+      // get a matcher object
+      Matcher m = p.matcher(sCurrentLine); 
+      sCurrentLine = m.replaceAll(REPLACE);
+      //System.out.println(sCurrentLine);
+      
+    
+      writer.write(sCurrentLine + "\n");
+    } 
+
+  } catch (IOException e) 
+    {
+      e.printStackTrace();
+    } 
+    finally 
+    {
+     
+    try 
+    {
+      writer.close();
+      if (br != null)br.close();
+    } catch (IOException ex) 
+      {
+	ex.printStackTrace();
+      }
+    }
+  }	
+}
